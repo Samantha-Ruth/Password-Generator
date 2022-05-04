b75d06920bb74db66aad8c0f9c262e39fb087910
Samantha-Ruth <sam.haberman@gmail.com>
2022-05-04 07:58:46 -0600

    Prompts created for length and character types.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,145 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+window.alert("Welcome to Password Generator! Please pick a few criteria for your password!");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+// Length: Prompt 
+   var passwordLength = function() {
+    var promptLength = window.prompt('How long would you like your password to be?  Please enter any length from 8 to 126 characters.'); 
+      if (promptLength === "" || promptLength=== null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        console.log(promptLength);
+  // **** NEED TO FIND OUT WHERE THIS NUMBER IS STORED AND THEN RETRIEVE IT BEFORE RUNNING IT THROUGH THE FOLLOWING CHECKER ***
+  //     if (promptLength < 8 || promptLength > 128)
+  //       window.alert("Please enter a number from 8 to 128.");
+  //       console.log(promptLength);
+  //     //use return to call it again and stop the rest of this function from running
+        return passwordLength();
+     }
+  }
 
+
+// UPPER CASE LETTERS: PROMPT
+   //Potentially write a prompt asking if upper, lower case or both. 
+var upperCase = function() {
+  var promptUpper = window.prompt('Would you like to include upper case letters? Enter "Yes" or "No".');
+    if (promptUpper === "" || promptUpper === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+    //use return to call it again and stop the rest of this function from running
+     return upperCase();
+   }
+  // convert promptFight to all lowercase so we can check with less options
+    promptUpper = promptUpper.toLowerCase();
+
+    if (promptUpper=== "yes") {
+   // Varify player has chosen to include upper case letters in their code.
+    window.alert("You have chosen to include capital letters in your password.");
+  }
+  else {
+    window.alert("Your password will not have any capital letters.");
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// LOWER CASE LETTERS: PROMPT
+   //Potentially write a prompt asking if upper, lower case or both. 
+   var lowerCase = function() {
+    var promptLower = window.prompt('Would you like to include lower case letters? Enter "Yes" or "No".');
+      if (promptLower === "" || promptLower === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+      //use return to call it again and stop the rest of this function from running
+       return lowerCase();
+     }
+    // convert promptFight to all lowercase so we can check with less options
+      promptLower = promptLower.toLowerCase();
+  
+      if (promptLower=== "yes") {
+     // Varify player has chosen to include upper case letters in their code.
+      window.alert("You have chosen to include lowercase letters in your password.");
+    }
+    else {
+      window.alert("Your password will not have any lowercase letters.");
+    }
+  }
+
+// INTEGERS: PROMPT
+
+var includeIntegers = function() {
+  var promptIntegers = window.prompt('Would you like to include integers? Enter "Yes" or "No".');
+    if (promptIntegers === "" || promptIntegers === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+    //use return to call it again and stop the rest of this function from running
+     return includeIntegers();
+   }
+  // convert promptFight to all lowercase so we can check with less options
+    promptIntegers = promptIntegers.toLowerCase();
+
+    if (promptIntegers=== "yes") {
+   // Varify player has chosen to include upper case letters in their code.
+    window.alert("You have chosen to include integers in your password.");
+  }
+  else {
+    window.alert("Your password will not have any integers.");
+  }
+}
+
+// special characters prompt
+
+var includeCharacters = function() {
+  var promptCharacters = window.prompt('Would you like to include special characters? Enter "Yes" or "No".');
+    if (promptCharacters === "" || promptCharacters === null) {
+      window.alert("You need to provide a valid answer! Please try again.");
+    //use return to call it again and stop the rest of this function from running
+     return includeCharacters();
+   }
+  // convert promptFight to all lowercase so we can check with less options
+    promptCharacters = promptCharacters.toLowerCase();
+
+    if (promptCharacters=== "yes") {
+   // Varify player has chosen to include upper case letters in their code.
+    window.alert("You have chosen to include special characters in your password.");
+  }
+  else {
+    window.alert("Your password will not have any special characters.");
+  }
+}
+
+passwordLength ();
+upperCase ();
+lowerCase ();
+includeIntegers ();
+includeCharacters ();
+
+
+
+
+
+
+
+
+// ***** // Assignment Code
+// ***** var generateBtn = document.querySelector("#generate");
+
+// ***** // Write password to the #password input
+// ******* function writePassword() {
+//  **** var password = generatePassword();
+//   ******* var passwordText = document.querySelector("#password");
+
+//  ****** passwordText.value = password;
+
+// ******}
+
+
+// ******// Add event listener to generate button
+// ****** generateBtn.addEventListener("click", writePassword);
+
+
+
+
+// var upperCase= function() {
+//   // ask player if they'd like to include uppercase letters
+//   var promptUpper = window.prompt('Would you like to include upper case letters? Enter "Yes" or "No".');
+
+//   // validate prompt answer (** CAN I WRITE A CODE FOR THIS? PROMPTNULL? )
+
+//   else {
+//     // if (promptUpper === "Yes"), store uppercase letters in array
+//     // else do nothing and continue on to the next window prompt. 
+//   }
---
+++ trial.js
@@ -0,0 +1,34 @@
+// Create Uppercase Array
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const capitalLetters = alpha.map((x) => String.fromCharCode(x));
+console.log(capitalLetters);
+
+// Create Lowercase Array
+const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
+const smallLetters = alpha2.map((x) => String.fromCharCode(x));
+console.log(smallLetters);
+
+// Create Integer Array
+const numbers = Array.from(Array(10)).map((e, i) => i + 48);
+const integers = numbers.map((x) => String.fromCharCode(x));
+console.log(integers);
+
+//  Create Character Array
+
+const character1 = Array.from(Array(6)).map((e, i) => i + 33);
+const firstCharacters = character1.map((x) => String.fromCharCode(x));
+
+/// and more characters
+const character2 = Array.from(Array(6)).map((e, i) => i + 58);
+const secondCharacters = character2.map((x) => String.fromCharCode(x));
+
+// and more
+const character3 = Array.from(Array(6)).map((e, i) => i + 91);
+const thirdCharacters = character3.map((x) => String.fromCharCode(x));
+
+const character4 = Array.from(Array(3)).map((e, i) => i + 123);
+const fourthCharacters = character4.map((x) => String.fromCharCode(x));
+
+const characterArray = firstCharacters.concat( secondCharacters,thirdCharacters,fourthCharacters);
+console.log(characterArray);
+// now have Uppercase Array, Lowercase Array, Integer Array, characterArray, 
