7475994a50d7cd7603e044ec1398b84c555041af
Samantha-Ruth <sam.haberman@gmail.com>
2022-05-08 10:57:24 -0600

    validation at the end

--- Develop/script.js
+++ Develop/script.js
@@ -29,28 +29,40 @@ var passwordRandom = [];
       return generatePassword();
     }  
   /// NEED TO SET UP A CONDITION IF PEOPLE ENTER LETTERS INSTEAD OF NUMBERS! // 
-  var upperCase = confirm("Click 'OK' if you'd like to include upper case letters. If you don't want to include caps, click cancel.");
+  var upperCase = confirm("Click 'OK' if you'd like to include uppercase letters. If you don't want to include uppercase letters, click Cancel.");
     // if user requests upperCase letters, add to passwordArray
     if (upperCase) {
       const alpha = Array.from(Array(26)).map((e, i) => i + 65);
       const capitalLetters = alpha.map((x) => String.fromCharCode(x));
       passwordArray = passwordArray.concat(capitalLetters);
+      var alertUpperCase = "Uppercase Letters\n";
     }
-  var lowerCase = confirm("Click 'OK' if you'd like to include lower case letters.");
+    else if (!upperCase) {
+      var alertUpperCase = "";
+    }
+  var lowerCase = confirm("Click 'OK' if you'd like to include lowercase letters. If you don't want to include lowercase letters, click Cancel.");
     // if user requests lowerCase letters, add to passwordArray
     if (lowerCase) {
       const alpha2 = Array.from(Array(26)).map((e, i) => i + 97);
       const smallLetters = alpha2.map((x) => String.fromCharCode(x));
       passwordArray = passwordArray.concat(smallLetters);
+      var alertLowerCase = "Lowercase Letters\n";
     }
-  var integers = confirm("Click 'OK' if you'd like to include integers.");
+    else if (!lowerCase) {
+      var alertLowerCase = "";
+    }
+  var integers = confirm("Click 'OK' if you'd like to include integers. If you don't want to include integers, click Cancel");
     // if user requests integers, add to passwordArray
     if (integers) {
       const numbers = Array.from(Array(10)).map((e, i) => i + 48);
       const integers = numbers.map((x) => String.fromCharCode(x));
       passwordArray = passwordArray.concat(integers);
+      var alertIntegers = "Integers\n";
       }
-  var characters = confirm("Click 'OK' if you'd like to include characters.");
+    else if (!integers) {
+      var alertIntegers = "";
+      }
+  var characters = confirm("Click 'OK' if you'd like to include characters. If you don't want to include characters, click Cancel.");
     if (characters) {
       const character1 = Array.from(Array(6)).map((e, i) => i + 33);
       const firstCharacters = character1.map((x) => String.fromCharCode(x));
@@ -67,7 +79,17 @@ var passwordRandom = [];
       const characters = firstCharacters.concat( secondCharacters,thirdCharacters,fourthCharacters);
       // if user requests characters, add all characters to passwordArray
       passwordArray = passwordArray.concat(characters);
+      var alertCharacters = "Characters\n";
     }
+    else if (!characters) {
+      var alertCharacters = "";
+    }
+
+
+    window.alert("You have chosen to include the following in your password:\n\n" + alertUpperCase + alertLowerCase + alertIntegers + alertCharacters);  
+
+
+
   if (!characters && !integers && !lowerCase && !upperCase) {
     window.alert("You must include at least one character type.");
     return generatePassword();
